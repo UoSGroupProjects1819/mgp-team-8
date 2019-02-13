2f0b32f059f28148c31e68bd558b582be5bce3c6
louisvagner <43639349+louisvagner@users.noreply.github.com>
2019-02-13 14:44:19 +0000

    First Commit of Unity Project

---
+++ Unity project/RogueLike - GroupProject Game/Assets/Scripts/GridController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+
+    public Vector3 cellSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
