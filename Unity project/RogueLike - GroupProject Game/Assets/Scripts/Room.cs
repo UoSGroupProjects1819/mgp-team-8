588b34f2acf77c711c74f13015881977121a4e98
unknown <petrut1997@yahoo.com>
2019-02-19 14:35:52 +0000

    Added room generation script. Improved the functionality of Room class.

--- Unity project/RogueLike - GroupProject Game/Assets/Scripts/Room.cs
+++ Unity project/RogueLike - GroupProject Game/Assets/Scripts/Room.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+    //Idea about room navigation keeping track of each room's neighbor and direction
+    public enum NeighborDrection
+    {
+        N, S, E, W
+    }
+    public Dictionary<NeighborDrection, Room> neighbors;
+
     public Grid grid { get; private set; }
+
+    public GameObject[][] tiles;
 
     private void Start()
     {
         grid = GetComponentInChildren<Grid>();
+        neighbors = new Dictionary<NeighborDrection, Room>();
+    }
+
+    public void SetupTileArray(int width, int height)
+    {
+        tiles = new GameObject[width][];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                tiles[i] = new GameObject[height];
+            }
+        }
     }
 }
