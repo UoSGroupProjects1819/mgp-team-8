5352ef26febabc1cddf963897ec9944554a02ade
louisvagner <43639349+louisvagner@users.noreply.github.com>
2019-02-19 17:44:19 +0000

    script that make the inventory appear and disappear

--- Unity project/RogueLike - GroupProject Game/Assets/Scripts/InventoryController.cs
+++ Unity project/RogueLike - GroupProject Game/Assets/Scripts/InventoryController.cs
@@ -3,45 +3,15 @@ using System.Collections;
 
 public class InventoryController : MonoBehaviour
 {
-    //[SerializeField]
-    //private GameObject[] Inventory;
-    //private bool visible;
+    public GameObject inventory; // Assign in inspector
+    private bool isShowing;
 
-    //public bool Visible
-    //{
-    //    get { return visible; }
-    //}
-
-    //public void Show()
-    //{
-    //    foreach (GameObject item in Inventory)
-    //    {
-    //        item.SetActive(true);
-    //    }
-    //}
-
-    //public void Hide()
-    //{
-    //    foreach (GameObject item in Inventory)
-    //    {
-    //        item.SetActive(false);
-    //    }
-    //}
-
-    //// Start is called before the first frame update
-    //void Start()
-    //{
-
-    //}
-
-    //// Update is called once per frame
-    //void Update()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.I))
-    //        if (UICollection.Visible)
-    //            UICollection.Hide();
-    //        else
-    //            UICollection.Show();
-    //}
-
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            isShowing = !isShowing;
+            inventory.SetActive(isShowing);
+        }
+    }
 }
